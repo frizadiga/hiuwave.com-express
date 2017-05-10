d5235d3d7a9f4c86dff149e9a9cdd94a13f53eb3
frizadiga <frizadiga@gmail.com>
2017-05-11 00:24:58 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hiuWave.com' });
+  res.render('index', { title: 'hiuWave' });
 });
 
 module.exports = router;
